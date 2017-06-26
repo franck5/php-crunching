2af493a0b50b60e3f0c35018b0e53513c353ba33
dubech <franck.dubech@hotmail.com>
2017-06-26 11:09:28 +0200

    initial commit

---
+++ Exo1.php
@@ -0,0 +1,42 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h2>Combien de mots contient ce dictionnaire ?</h2>
+	<?php 
+
+		$string = file_get_contents("fr-classique.dic", FILE_USE_INCLUDE_PATH);
+		$dico = explode("\n", $string);
+
+		$result = count($dico);
+		echo "Ce dictionnaire contient $result mots <br>";
+
+	?>
+	<h2>Combien de mots font exactement 15 caractères ?</h2>
+	<?php
+		$string_15 = array();
+		foreach ($dico as $recup) {
+			if (strlen($recup) == 15) {
+				array_push($string_15, $recup);
+			}
+		}
+		echo count($string_15)." mots font exactement 15 caracteres <br>";
+	?>
+	<h2>Combien de mots contiennent la lettre « w » ?</h2>
+	<?php
+		$string_w = array();
+			foreach ($dico as $stock) {
+				if (strpos($stock, "w") !== false) {
+					array_push($string_w, $stock);
+				}
+			}
+			echo count($string_w)." mots contiennent un w";
+	?>
+	<h2>Combien de mots finissent par la lettre « q » ?</h2>
+</body>
+</html>
+
+
